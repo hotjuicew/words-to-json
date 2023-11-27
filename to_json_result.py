343954caf553959e95427bf7d91233b8e9282851
Jiaying Wu <imjiayingzh@outlook.com>
2023-11-27 20:59:15 +0800

    完成

--- to_json_result.py
+++ to_json_result.py
@@ -2,7 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import json
-# 将json格式的，添加音标
+
+
+# 将json格式的，添加音标和意思
 def SearchWords(Word):
     try:
         url = 'https://www.youdao.com/result?word='+Word+'&lang=en' 
@@ -25,7 +27,6 @@ def RegularFind(soup):
         for li in meaningsLi[:2]:
             text_parts = [span.get_text() for span in li.find_all('span')]
             combined_text = ''.join(text_parts)
-            print("combined_text",combined_text)
             meanings.append(combined_text)
     else:
         meanings=["没找到释义"]
@@ -42,23 +43,47 @@ def RegularFind(soup):
 
 if __name__ == '__main__':
 
-    with open('output.json', 'r', encoding='utf-8') as f:
-        data = json.load(f)
+    # 将提取的英文单词写入到words.txt文件中
+    with open('raw.txt', 'r', encoding='utf-8') as file:
+        text = file.read()
+    english_words = re.findall(r'\b[A-Za-z]+\b', text)
+    word_counts = {}
+    for word in english_words:
+        if len(word) > 2:  # 排除长度为1或2的单词
+            word_counts[word] = word_counts.get(word, 0) + 1
+    unique_words = [word for word, count in word_counts.items() if count == 1]
+    # 将只出现一次的英文单词写入到words.txt文件中
+    with open('words.txt', 'w', encoding='utf-8') as output_file:
+        for word in unique_words:
+            output_file.write(word + '\n')
+
+    # 将干净的txt（每行一个单词）初步转为json
+    with open('words.txt', 'r', encoding='utf-8') as source_file:
+        lines = source_file.readlines()
+    data = []
+    for line in lines:
+        word = line.strip() 
+        if not word:
+            continue 
+        data.append({
+            "name": word,
+            "trans": "",
+            "usphone": "",
+            "ukphone": ""
+        })
 
     num=1
     for item in data:
         Word = item['name']
-        print("Word",Word)
         num=num+1
         print(num+1)
         meanings, PhoneticSymbolUK, PhoneticSymbolUS = SearchWords(Word)
-        print("meanings",meanings)
         item['trans'] = meanings
         item['ukphone'] = PhoneticSymbolUK
         item['usphone'] = PhoneticSymbolUS
 
 
-    with open('new_data.json', 'w', encoding='utf-8') as f:
+    with open('results.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
     print('Phonetic symbols added to the JSON data and saved in updated_data.json.')
